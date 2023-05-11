cf0abbff4ccf8c9fa16c9debabfaa81354fea510
Valentin <72602932+valentinuus@users.noreply.github.com>
2023-05-11 06:21:21 +0300

    Add files via upload

--- js/script-main.js
+++ js/script-main.js
@@ -10,17 +10,20 @@ let swiperSliderImg = new Swiper(sliderImg, {
 let swiperSliderText = new Swiper(sliderText, {
 	spaceBetween: 200,
 	loop: false,
+	thumbs: {
+		swiper: swiperSliderImg,
+	},
 	navigation: {
 		nextEl: '.swiper-button-next',
 		prevEl: '.swiper-button-prev',
 	},
 });
 
-swiperSliderImg.on('slideChange', function () {
-	swiperSliderText.slideTo(swiperSliderImg.realIndex, 0);
-});
+// swiperSliderImg.on('slideChange', function () {
+// 	swiperSliderText.slideTo(swiperSliderImg.realIndex, 0);
+// });
 
-swiperSliderText.on('slideChange', function () {
-	swiperSliderImg.slideTo(swiperSliderText.realIndex, 0);
-});
+// swiperSliderText.on('slideChange', function () {
+// 	swiperSliderImg.slideTo(swiperSliderText.realIndex, 0);
+// });
 // slider
