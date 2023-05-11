3645266003796260a0a4f96e19db48dd633cfb49
Valentin <72602932+valentinuus@users.noreply.github.com>
2023-05-11 06:34:24 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -70,3 +70,24 @@ if (iconMenu) {
 }
 // Меню бургер
 
+
+const sliderImg = document.querySelector('.img-slider__wrap');
+const sliderText = document.querySelector('.text-slider__wrap');
+
+let swiperSliderImg = new Swiper(sliderImg, {
+	spaceBetween: 10,
+	loop: false,
+});
+
+let swiperSliderText = new Swiper(sliderText, {
+	spaceBetween: 200,
+	loop: false,
+	thumbs: {
+		swiper: swiperSliderImg,
+	},
+	navigation: {
+		nextEl: '.swiper-button-next',
+		prevEl: '.swiper-button-prev',
+	},
+});
+
